65996b13a5a90e06ebf3f7b505c204e5d47f1251
Aaminamaxamed <aaminamaxamed630@gmail.com>
2024-11-04 23:05:53 +0300

    Add files via upload

---
+++ TASK MANAGER/routes/taskRoutes.js
@@ -0,0 +1,19 @@
+const taskRoutes =(req,res)=>{
+    if(req.method =='GET'){
+        getTasks(req,res);
+    
+    }else if(req.method== 'POST'){
+        createTasks(req,res)
+    }else if (req.method =='PATCH'){
+        updateTask(req,res)
+    }else if(req.method == 'DELETE'){
+        deleteTask(req.res)
+    } else{
+        res.writeHead(404, 'data not found', {'content-type': 'applcation/json'})
+        res.end(JSON.stringify({
+            message: 'unknown method required.'
+        }))
+    }
+}
+
+module.exports = taskRoutes;
